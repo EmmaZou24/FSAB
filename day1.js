50671887c29ffd249d91eaf4e5179a326e7e797d
EmmaZou24 <emmabff7@gmail.com>
2025-09-08 22:41:57 -0400

    day one kinda...still need styling

---
+++ day1.js
@@ -0,0 +1,16 @@
+let imgs = ["https://i.imgur.com/Pa94msC.jpg", 
+    "https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?w=2000&t=st=1694473438~exp=1694474038~hmac=7348cb720086d87666200b4b9a3959263726f1fe0d399f77622255517a4d809c", 
+    "https://img.freepik.com/free-vector/beautiful-watercolor-background_23-2148486848.jpg?w=2000&t=st=1694473502~exp=1694474102~hmac=07dcf8f6e518b9402e2367286beda82b778ebc0680c8a000f6fc0bd0db5326e0"];
+
+let currInd = 0;
+
+function increment() {
+  let img = imgs[currInd];
+  currInd++;
+  if (currInd>=imgs.length) {currInd=0;}
+  return img;
+}
+
+function handleClick() {
+  document.getElementById("cardImg").src = increment();
+}
